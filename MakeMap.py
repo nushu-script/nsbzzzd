a882f9d90f38b226e244d3daf1ae95fefb2e0f2f
sgalal <32255369+chromezh@users.noreply.github.com>
2019-09-10 21:30:06 +0800

    Create table.json

--- MakeMap.py
+++ MakeMap.py
@@ -4,10 +4,15 @@ import pandas
 from collections import defaultdict
 import json
 
-d = defaultdict(list)
+d1 = defaultdict(list)
+d2 = []
 data = pandas.read_csv('data.csv')
+
 for v1, v2, ks in zip(data['女书序号'], data['发音'], data['对应汉字']):
-    for k in ks:
-        d[k].append((v1, v2))
+	d2.append((v1, v2, ks))
+	for k in ks:
+		d1[k].append((v1, v2))
 with open('map.json', 'w') as fout:
-	print(json.dumps(d, ensure_ascii=False, sort_keys=True).replace('], "', ']\n,"').replace(' ', ''), file=fout)
+	print(json.dumps(d1, ensure_ascii=False, sort_keys=True).replace('], "', ']\n,"').replace(' ', ''), file=fout)
+with open('table.json', 'w') as fout:
+	print(json.dumps(d2, ensure_ascii=False, sort_keys=True).replace('], [', ']\n,[').replace(' ', ''), file=fout)
